fd70b0335648a68d539e4b12d2175b37a10c8557
gezever <gezever@skynet.be>
2022-09-06 15:27:36 +0200

    lijst van fysico-chemische eigenschappen op basis van de lijst van parameters van VMM

--- src/main/R/csv_to_json.R
+++ src/main/R/csv_to_json.R
@@ -2,14 +2,14 @@
 library(tidyr)
 library(dplyr)
 library(jsonlite)
-setwd("/home/gehau/git/codelijst-fysico-chemische_parameter/src/main/R")
 
-df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/fysico-chemische_parameter.csv", sep=",", na.strings=c("","NA"))
 
-tco <- subset(df, topConceptOf == 'https://data.omgeving.vlaanderen.be/id/conceptscheme/fysico-chemische_parameter' ,
+df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch.csv", sep=",", na.strings=c("","NA"))
+
+tco <- subset(df, topConceptOf == 'https://data.omgeving.vlaanderen.be/id/conceptscheme/fysico-chemisch' ,
                    select=c(uri, topConceptOf))
 htc <- as.list(tco["uri"])
-df2 <- data.frame('https://data.omgeving.vlaanderen.be/id/conceptscheme/fysico-chemische_parameter', htc)
+df2 <- data.frame('https://data.omgeving.vlaanderen.be/id/conceptscheme/fysico-chemisch', htc)
 names(df2) <- c("uri","hasTopConcept")
 df <- bind_rows(df, df2)
 
@@ -17,9 +17,9 @@ df <- bind_rows(df, df2)
 df <- df %>%
   rename("@id" = uri,
          "@type" = type)
-write.csv(df,"../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/fysico-chemische_parameter_separate_rows.csv", row.names = FALSE)
-context <- jsonlite::read_json("../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/context.json")
+write.csv(df,"../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch_separate_rows.csv", row.names = FALSE)
+context <- jsonlite::read_json("../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/context.json")
 df_in_list <- list('@graph' = df, '@context' = context)
 df_in_json <- toJSON(df_in_list, auto_unbox=TRUE)
-write(df_in_json, "/tmp/fysico-chemische_parameter.jsonld")
+write(df_in_json, "/tmp/fysico-chemisch.jsonld")
 
--- src/main/bash/csv_to_rfd.sh
+++ src/main/bash/csv_to_rfd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/fysico-chemische_parameter.csv
-# to jsonld, /tmp/fysico-chemische_parameter.jsonld
+# Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch.csv
+# to jsonld, /tmp/fysico-chemisch.jsonld
 Rscript ../R/csv_to_json.R
 
 # Make formatted jsonld and turtle
-riot --formatted=TURTLE /tmp/fysico-chemische_parameter.jsonld   > '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/fysico-chemische_parameter.ttl'
-riot --formatted=JSONLD '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/fysico-chemische_parameter.ttl'   > '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemische_parameter/fysico-chemische_parameter.jsonld' 
+riot --formatted=TURTLE /tmp/fysico-chemisch.jsonld   > '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch.ttl'
+riot --formatted=JSONLD '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch.ttl'   > '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch.jsonld' 
 
