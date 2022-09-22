c8a48be765370a26038a46983bb31bf4943722cd
gezever <gezever@skynet.be>
2022-09-22 15:20:26 +0200

    dcat op dataset classpath

--- src/main/R/dcat-from-csv.R
+++ src/main/R/dcat-from-csv.R
@@ -4,7 +4,6 @@ library(dplyr)
 library(jsonlite)
 library(data.table)
 library(stringr)
-#setwd('/home/gehau/git/codelijst-emissie/src/main/R')
 
 artifactory <- "https://repo.omgeving.vlaanderen.be/artifactory/release"
 
@@ -25,12 +24,12 @@ package_id <- paste("omg_package", packageName_, sep = ":")
 downloadLocation_ <- paste(artifactory, class_path, name, version_former_release, packageFileName_, sep = "/")
 
 
-df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/catalog/catalog.csv", sep=",", na.strings=c("","NA"))
+df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/dataset/codelijst-fysico-chemische-eigenschap/catalog.csv", sep=",", na.strings=c("","NA"))
 setDT(df)[id == package_id, downloadLocation := downloadLocation_]
 setDT(df)[id == package_id, packageFileName := packageFileName_]
 setDT(df)[id == package_id, packageName := packageName_]
 setDT(df)[id == package_id, versionInfo := version_former_release]
-write.csv(df,"../resources/be/vlaanderen/omgeving/data/id/catalog/catalog.csv", row.names = FALSE)
+write.csv(df,"../resources/be/vlaanderen/omgeving/data/id/dataset/codelijst-fysico-chemische-eigenschap/catalog.csv", row.names = FALSE)
 df <- df %>%
   mutate_all(list(~ str_c("", .)))
 for(col in 1:ncol(df)) {   # for-loop over columns
@@ -41,10 +40,10 @@ df <- df %>% rename(
   "@id" = id,
   "@type" = type
 )
-context <- jsonlite::read_json("../resources/be/vlaanderen/omgeving/data/id/catalog/context.json")
+context <- jsonlite::read_json("../resources/be/vlaanderen/omgeving/data/id/dataset/codelijst-fysico-chemische-eigenschap/context.json")
 df_in_list <- list('@graph' = df, '@context' = context)
 df_in_json <- toJSON(df_in_list, auto_unbox=TRUE)
 write(df_in_json, "/tmp/catalog.jsonld")
-system("riot --formatted=TURTLE /tmp/catalog.jsonld > ../resources/be/vlaanderen/omgeving/data/id/catalog/catalog.ttl")
-system("riot --formatted=JSONLD ../resources/be/vlaanderen/omgeving/data/id/catalog/catalog.ttl > ../resources/be/vlaanderen/omgeving/data/id/catalog/catalog.jsonld")
+system("riot --formatted=TURTLE /tmp/catalog.jsonld > ../resources/be/vlaanderen/omgeving/data/id/dataset/codelijst-fysico-chemische-eigenschap/catalog.ttl")
+system("riot --formatted=JSONLD ../resources/be/vlaanderen/omgeving/data/id/dataset/codelijst-fysico-chemische-eigenschap/catalog.ttl > ../resources/be/vlaanderen/omgeving/data/id/dataset/codelijst-fysico-chemische-eigenschap/catalog.jsonld")
 
