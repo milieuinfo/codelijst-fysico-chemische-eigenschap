daa7546099d99f4eddfbde2fb9a9245ae1d00667
gezever <gezever@skynet.be>
2024-03-25 13:20:20 +0100

    javascript

--- src/main/bash/csv_to_rfd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/fysico-chemisch/fysico-chemisch.csv
-# to jsonld, /tmp/fysico-chemisch.jsonld
-Rscript ../R/csv_to_json.R
-
